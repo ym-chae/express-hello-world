94c544bfb32645a1d4c74920129e9ec07896059b
ymchae <>
2023-04-07 16:56:01 +0900

    111

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.get('/fnb', function(request, response) {
+  response.sendFile(__dirname +  '/test.html');
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
